e5b7b2458e71fa2d1755afd64b36a227c3b864fc
rakib <rakib.hasan814563@gmail.com>
2023-11-29 20:22:57 +0600

    All activity route fixed

--- app/routes/baby_care/allHistory.js
+++ app/routes/baby_care/allHistory.js
@@ -32,22 +32,24 @@ const getCombinedBabyData = async (req, res, next) => {
 
     // Access related data using babyData
     const combinedData = [];
-
-    // res.status(200).json({ data: babyData });
-
+    const pushFunc = (table) => {
+      if (babyData[table]) {
+        let Objects = babyData[table].map((obj) => ({
+          ...obj.toJSON(),
+          type: table,
+        }));
+        combinedData.push(...Objects);
+      }
+    };
     // Push related data from other tables
-    if (babyData.baby_sleeps) combinedData.push(...babyData.baby_sleeps);
-    if (babyData.baby_breast_pumpings)
-      combinedData.push(...babyData.baby_breast_pumpings);
-    if (babyData.baby_diapers) combinedData.push(...babyData.baby_diapers);
-    if (babyData.baby_feeds) combinedData.push(...babyData.baby_feeds);
-    if (babyData.baby_medications)
-      combinedData.push(...babyData.baby_medications);
-    if (babyData.baby_notes) combinedData.push(...babyData.baby_notes);
-    if (babyData.baby_symptoms) combinedData.push(...babyData.baby_symptoms);
-    if (babyData.baby_temperatures)
-      combinedData.push(...babyData.baby_temperatures);
-    // ... push other related data in a similar way
+    pushFunc("baby_sleeps");
+    pushFunc("baby_breast_pumpings");
+    pushFunc("baby_diapers");
+    pushFunc("baby_feeds");
+    pushFunc("baby_medications");
+    pushFunc("baby_notes");
+    pushFunc("baby_symptoms");
+    pushFunc("baby_temperatures");
 
     // Sort the combined array by createdAt
     combinedData.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
