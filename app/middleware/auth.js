f5dcba279721a2256bcd5b2f4418ca6be929f9d5
rumonkh0 <rummankh0@gmail.com>
2024-02-13 23:55:10 +0600

    return res status

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -15,7 +15,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     // Set token from Bearer token in header
-    token = req.headers.authorization.split(" ")[1];
+    token = req.headers.authorization.split(" ")[1];kali
   } else if (req.cookies.token) {
     // Set token from cookie
     token = req.cookies.token;
