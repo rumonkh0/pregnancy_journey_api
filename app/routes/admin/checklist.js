edbd7b77f33ffe4bdb15c8e70eaa4534f32a409b
rumonkh0 <rummankh0@gmail.com>
2024-05-14 22:01:00 +0600

    up

--- app/routes/admin/checklist.js
+++ app/routes/admin/checklist.js
@@ -18,16 +18,18 @@ const ChecklistItem = require("../../models/tools/mother/ChecklistItem");
 router.use(protect, authorize("superadmin", "admin", "user"));
 // router.use('/:motherId/babylist', babyListRouter);
 
-// Route to get all users
+// nav main items
 router.get("/item", getAllItem(ChectlistItem));
 router.post("/item", createItem(ChecklistItem));
+
+//all subitems and its checkitems under a item
 router.get("/subitem/:item", getOneItem(ChectlistSubItem));
 router.post("/subitem", createSubItem(ChectlistSubItem));
+
+
 router.get("/:subitem", getOneSubItem(Chectlist));
+
+//create checklist
 router.post("/", createOne(Chectlist));
-// router.get("/:userId", getUser);
-// router.post("/", createUser);
-// router.put("/:userId", upload.single("user_image_field"), updateUser);
-// router.delete("/:userId", deleteUser);
 
 module.exports = router;
